e523df72708d403f665c911dfa1a0b4a9ad9d28d
Ankush <mondalankush9851@gmail.com>
2025-11-08 23:30:41 +0530

    minor changes

--- src/constants.zig
+++ src/constants.zig
@@ -1,8 +1,9 @@
 // constants
-
 pub const file_path = "/var/log/syslog";
-pub const file_dir = "/var/log";
+pub const dir = "/var/log";
 pub const MIN_BUFFER_SIZE = 262144;
 pub const BUFFER_SIZE = 1048576;
 pub const MAX_EVENTS = 1000;
 pub const MAX_PATH_LEN = 1024;
+
+pub var content_buffer: [BUFFER_SIZE]u8 = undefined;
