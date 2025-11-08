60981961ce011cfe9dab3095925fce459711d52c
Ankush <mondalankush9851@gmail.com>
2025-11-08 23:30:06 +0530

    added more macros of inotify and allocator

--- src/main.zig
+++ src/main.zig
@@ -1,23 +1,24 @@
 const std = @import("std");
 
+// This module contains inotify watch instance
 const wt = @import("watch.zig");
-const fp = @import("filepos.zig");
+// Handles the file parsing positions
+const fp = @import("filetracker.zig");
+// Contains all the predefined constant values
 const ct = @import("constants.zig");
 
 const linux = std.os.linux;
 
 pub fn main() !void {
-    // Minimal inotify implementation
-    var watch = try wt.Watcher.init(ct.file_dir);
+    const allocator = std.heap.page_allocator;
+
+    // Create an watch instance initialized with directory path
+    var watch = try wt.Watcher.init(allocator, ct.dir);
     defer watch.deinit();
 
-    // Get file stats
-    var conf = try fp.FilePos.init(ct.file_path);
-
-    // Inotify Buffer for reading events
-    var inotify_buffer: [wt.inotify_buffer_size]u8 align(8) = undefined;
-
-    var content_buffer: [ct.BUFFER_SIZE]u8 = undefined;
+    // Later to be improved
+    var tracker = try fp.FileTracker.init(ct.file_path);
+    defer tracker.deinit();
 
     // Epoll configuration
     const epfd = linux.epoll_create1(linux.EPOLL.CLOEXEC);
@@ -36,13 +37,13 @@ pub fn main() !void {
 
         for (events[0..ready]) |evt| {
             if (evt.data.fd == watch.inotify_fd) {
-                const len = linux.read(watch.inotify_fd, &inotify_buffer, wt.inotify_buffer_size); 
+                const len = linux.read(watch.inotify_fd, &wt.inotify_buffer, wt.inotify_buffer_size); 
 
                 var ptr: usize = 0;
                 while(ptr < len) {
-                    const event = @as(*const linux.inotify_event, @alignCast(@ptrCast(&inotify_buffer[ptr])));
-                    if (event.mask & (linux.IN.MODIFY | linux.IN.CLOSE_WRITE) != 0) {
-                        _ = try conf.readNewContent(&content_buffer);
+                    const event = @as(*const linux.inotify_event, @alignCast(@ptrCast(&wt.inotify_buffer[ptr])));
+                    if (event.mask & (linux.IN.MOVED_TO | linux.IN.DELETE | linux.IN.CREATE | linux.IN.MOVED_FROM | linux.IN.MODIFY | linux.IN.CLOSE_WRITE | linux.IN.MOVE_SELF | linux.IN.DELETE_SELF) != 0) {
+                        _ = try tracker.readNewContent(&ct.content_buffer);
                     }
                     
                     ptr += @sizeOf(linux.inotify_event) + event.len;
