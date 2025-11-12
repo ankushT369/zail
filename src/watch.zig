e095b351de5ada869cc34ac1f374021a9b00d5f5
Ankush <mondalankush9851@gmail.com>
2025-11-12 08:46:43 +0530

    handled integer overflow crashing!!

--- src/watch.zig
+++ src/watch.zig
@@ -46,8 +46,12 @@ pub const Watcher = struct {
         while (iter.next() catch null) |entry| {
             if (entry.kind == .directory and !std.mem.eql(u8, entry.name, ".") and !std.mem.eql(u8, entry.name, "..")) {
                 const next_path = std.fmt.bufPrintZ(&buf, "{s}/{s}", .{path, entry.name}) catch continue;
+                std.debug.print("path: {s}\n", .{next_path});
 
                 const ret = linux.inotify_add_watch(self.inotify_fd, next_path, linux.IN.MOVED_TO | linux.IN.DELETE | linux.IN.CREATE | linux.IN.MOVED_FROM);
+
+                if (ret >= std.math.maxInt(i32)) continue;
+
                 const wd: i32 = @intCast(ret);
 
                 if (wd >= 0) {
@@ -60,8 +64,12 @@ pub const Watcher = struct {
             }
             else if (entry.kind == .file) {
                 const next_path = std.fmt.bufPrintZ(&buf, "{s}/{s}", .{path, entry.name}) catch continue;
+                std.debug.print("path: {s}\n", .{next_path});
 
                 const ret = linux.inotify_add_watch(self.inotify_fd, next_path, linux.IN.MODIFY | linux.IN.CLOSE_WRITE | linux.IN.MOVE_SELF | linux.IN.DELETE_SELF);
+
+                if (ret >= std.math.maxInt(i32)) continue;
+
                 const wd: i32 = @intCast(ret);
 
                 if (wd >= 0) {
